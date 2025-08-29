414a89fb751f956f0041067d813b5b4ffd2d8ffb
Arfa K A <147141631+ArfaKA-03@users.noreply.github.com>
2025-08-29 17:53:48 +0530

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,2 +1,8 @@
 buttons.forEach(button => {
-    button.addEventListener("click", function () {}
+    button.addEventListener("click", function () {
+        let buttonText = this.innerText;
+
+        if (button.id === "all-clear") {
+            ipscreen.value = ""; // Clear the screen
+        }
+    }
