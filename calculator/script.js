a680133612ad5bc54fa3ec9b5ffb68a72ed33538
Arfa K A <147141631+ArfaKA-03@users.noreply.github.com>
2025-08-30 20:11:59 +0530

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,6 @@
+let ipscreen = document.getElementById("ipscreen");
+let ans = document.getElementById("ans");
+let buttons = document.querySelectorAll(".num button");
 buttons.forEach(button => {
     button.addEventListener("click", function () {
         let buttonText = this.innerText;
@@ -5,4 +8,8 @@ buttons.forEach(button => {
         if (button.id === "all-clear") {
             ipscreen.value = ""; // Clear the screen
         }
-    }
+        else if (buttonText === "%") {
+            ipscreen.value = parseFloat(ipscreen.value) / 100;
+        }
+    });
+});
