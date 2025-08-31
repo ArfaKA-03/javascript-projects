81d69d918eeda0a10caff1ad4abc2c56cd9384e0
Arfa K A <147141631+ArfaKA-03@users.noreply.github.com>
2025-08-31 12:51:51 +0530

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,11 @@
 let ipscreen = document.getElementById("ipscreen");
 let ans = document.getElementById("ans");
 let buttons = document.querySelectorAll(".num button");
+let delbtn = document.getElementById("del");
+let funct = document.getElementById("extra_func");
+let ac = document.getElementById("all-clear");
+let squre = document.getElementById("squr");
+
 buttons.forEach(button => {
     button.addEventListener("click", function () {
         let buttonText = this.innerText;
@@ -10,6 +15,25 @@ buttons.forEach(button => {
         }
         else if (buttonText === "%") {
             ipscreen.value = parseFloat(ipscreen.value) / 100;
+        }
+        else if (buttonText === "=") {
+            try {
+                ipscreen.value = eval(ipscreen.value);
+            }
+            catch {
+                ipscreen.value = "Error";
+            }
+        } else if (button.id === "del") {
+            ipscreen.value = ipscreen.value.slice(0, -1);
+        }
+        else if (button.id === "squr") {
+            let currentValue = parseFloat(ipscreen.value);
+            if (!isNaN(currentValue)) {
+                ipscreen.value = currentValue * currentValue; // square the number
+            }
+        }
+        else {
+            ipscreen.value += buttonText;
         }
     });
 });
